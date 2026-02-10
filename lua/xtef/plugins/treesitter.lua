d7d61e12fd16561e73554f9270d1f7ec6ca914f0
stef <offstefx@gmail.com>
2026-02-10 19:32:41 +0100

    cambios treesitter automatico al abrir archivo

--- lua/xtef/plugins/treesitter.lua
+++ lua/xtef/plugins/treesitter.lua
@@ -1,13 +1,34 @@
 return {
-    "nvim-treesitter/nvim-treesitter",
-    build = ":TSUpdate",
-    config = function()
-        local status_ok, configs = pcall(require, "nvim-treesitter.configs")
-        if not status_ok then return end
-        configs.setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "python", "rust", "javascript", "typescript" },
-            highlight = { enable = true },
-            indent = { enable = true },
-        })
-    end
+  "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter").install({
+      "javascript",
+      "typescript",
+      "c",
+      "lua",
+      "vim",
+      "vimdoc",
+      "query",
+      "rust",
+      "go",
+      "python",
+      "tsx",
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "*",
+      callback = function()
+        pcall(vim.treesitter.start)
+      end,
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      pattern = "*",
+      callback = function()
+        vim.bo.indentexpr = "v:lua.vim.treesitter.indentexpr()"
+      end,
+    })
+  end,
 }
